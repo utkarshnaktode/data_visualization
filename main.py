a739ef2557d25a460139420608d73b76c43a2608
utkarsh <utkarshnaktode40218@gmail.com>
2025-12-16 11:56:51 +0530

    2nd commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ax.legend(title='Fruit color')
 plt.savefig('bars.png', bbox_inches='tight')
 
 cat = ["bored", "happy", "happy", "happy", "happy", "bored"]
-dog = ["bored", "bored", "bored", "happy", "bored", "bored"]
+dog = ["happy", "bored", "bored", "happy", "bored", "bored"]
 activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
 
 fig, ax = plt.subplots()
